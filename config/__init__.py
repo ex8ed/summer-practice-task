276518bddd76506668751087bbd62b226dd039f2
ex8ed <an.proshak@gmail.com>
2023-07-25 17:07:46 +0300

    new packed files for GUI configurations

---
+++ config/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: UTF-8 -*-
+__all__ = ['style_settings']
+
+__version__ = "0.5.1"
+__author__ = "Nick Proshak"
+__licence__ = "MIT"
---
+++ config/style_settings.py
@@ -0,0 +1,11 @@
+# -*- coding: UTF-8 -*-
+"""Contains style settings for Qt widgets and layouts"""
+
+Q_MAIN_WINDOW_STYLE = """
+            QMainWindow {
+                background-color: #20420e;
+            }
+            QLabel {
+                color: white
+            }
+        """
