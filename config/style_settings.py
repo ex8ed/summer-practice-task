18fb99362e64b351ee45ec73e65b034fa316aa79
ex8ed <an.proshak@gmail.com>
2023-07-25 20:32:07 +0300

    new style settings for main window and other widgets

--- config/style_settings.py
+++ config/style_settings.py
@@ -3,9 +3,15 @@
 
 Q_MAIN_WINDOW_STYLE = """
             QMainWindow {
-                background-color: #20420e;
+                background-color: black;
             }
             QLabel {
-                color: white
+                color: white;
+            }
+            QLineEdit {
+                background-color: white;
+                color: #000000;
             }
         """
+
+Q_SIM_COMBO_BOX_WIDTH = 200
